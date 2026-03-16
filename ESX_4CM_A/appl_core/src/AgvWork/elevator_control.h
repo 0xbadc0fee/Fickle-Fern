f5535bc356bf3fa62657330f95cd1a7b1d0d067f
tiffanygohnert <tiffanygohnert@gmail.com>
2026-03-16 09:58:02 -0400

    FPC-149 Elevator Control Logic

--- ESX_4CM_A/appl_core/src/AgvWork/elevator_control.h
+++ ESX_4CM_A/appl_core/src/AgvWork/elevator_control.h
@@ -1,84 +1,94 @@
-/*! \file       elevator_control.h.h
+//-----------------------------------------------------------------------------
+/*! \file       elevator_control.h
     \brief      <description>
 
+    project     FloryTemplate_4CM
+    copyright   STW Technic (c) 2026
+    license     use only under terms of contract / confidential
 
-   	\implementation
-   	project     FloryTemplate_4CM
-   	copyright   STW Technic (c) 2026
-   	license     use only under terms of contract / confidential
+    created     Jan 6, 2026 STW Technic
+ */
 
-   	created     Jan 7, 2026 kyle.boch
-   	\endimplementation
-*/
 #ifndef APPL_CORE_SRC_AGVWORK_ELEVATOR_CONTROL_H_
 #define APPL_CORE_SRC_AGVWORK_ELEVATOR_CONTROL_H_
 
-/* -- Includes ------------------------------------------------------------------------------------------------------ */
 #include "stwtypes.h"
 
 #include "hmi_definition.h"
+#include "input_handler_lib.h"
+#include "output_handler_lib.h"
+#include "helper_control.h"
 
 /* -- Defines ------------------------------------------------------------------------------------------------------- */
 #define ELEVATOR_ON  (1u)
 #define ELEVATOR_OFF (0u)
 
+#define ELEVATOR_MIN_CURRENT_A  (0.0F)
+#define ELEVATOR_MAX_CURRENT_A  (10000.0F)
+
 /* -- Types --------------------------------------------------------------------------------------------------------- */
 
-/** \brief Checkpoints Structure - Elevator Control
- *
- * This structure represents all checkpoints that are relevant
- * to elevator control
- */
+/** \brief Checkpoints Structure - Elevator Control */
 typedef struct
 {
-    uint8 u8_chkPoint1;                    //!<Checkpoint #1
-    uint8 u8_chkPoint2;                    //!<Checkpoint #2
-    uint8 u8_chk3;                         //!<Checkpoint #3
+        uint8 u8_chkPoint1;                    //!<Checkpoint #1
+        uint8 u8_chkPoint2;                    //!<Checkpoint #2
+        uint8 u8_chk3;                         //!<Checkpoint #3
+        uint8 u8_chkPoint1;                    //!<Checkpoint #1
+        uint8 u8_chkPoint2;                    //!<Checkpoint #2
+        float32 f32_chkPoint1;                   //!<Checkpoint #3
 
 }T_ChkPoints_Elevator;
 
 
-/** \brief Configuration Structure - Elevator Control
- *
- * This structure represents all NVM configuration variables
- * that are relevant to elevator control
- */
 typedef struct
 {
-    uint8 u8_minSpeed;                      //!<Configuration Parameter 1
-    uint8 u8_maxSpeed;                      //!<Configuration Parameter 2
+        uint8 u8_minSpeed;                      //!<Configuration Parameter 1
+        uint8 u8_maxSpeed;                      //!<Configuration Parameter 2
+        float32 f32_max_current;           //rename f32_valve_threshold_A           //!<Configuration Parameter 1
 
 }T_Config_Elevator;
 
-/** \brief Control Structure - Elevator Control
- *
- * This structure represents all variables and pointers that
- * are utilized and tracked for elevator control that need to
- * persist through cyclic calls (static).
- *
- * This structure does not include any variables that are considered
- * temporary.
- */
+
 typedef struct
 {
-    //Local Control Variables
-    uint8 u8_onOffCommand;                  //!<Local On Off Command Variable
-    uint8 u8_speedCommand;                  //!<Local Speed Command Variable
-    uint8 u8_speedFeedback;                 //!<Local Speed Feedback Variable
+        //Local Control Variables
+        uint8 u8_onOffCommand;                  //!<Local On Off Command Variable
+        uint8 u8_speedCommand;                  //!<Local Speed Command Variable
+        uint8 u8_speedFeedback;                 //!<Local Speed Feedback Variable
+        //Local Control Variables
+        uint8 u8_elevator_enabled;
 
-    //TX CAN Variables
-    uint8 *pu8_elevatorStatus;              //!<On/Off Status of Elevator (To Display)
-    uint8 *pu8_elevatorButtonColor;         //!<Button color code for the Elevator On/Off button (To Button Panel)
+        //TX CAN Variables
+        uint8 *pu8_elevatorStatus;              //!<On/Off Status of Elevator (To Display)
 
-    //RX CAN Variables
-    uint8 *pu8_requestedSpeed;              //!<Elevator Requested Speed (From Display)
-    uint8 *pu8_onOffCommand;                //!<Elevator On/Off Command (From Joystick)
+        //RX CAN Variables
+        uint8 *pu8_requestedSpeed;              //!<Elevator Requested Speed (From Display)
+        uint8 *pu8_onOffCommand;                //!<Elevator On/Off Command (From Joystick)
 
-    //NVM Configuration Parameters
-    T_Config_Elevator *pt_nvmElevator;      //!<Elevator Control Configuration Structure
+        //TX CAN Variables
+        uint8 *pu8_elevatorStatus;              //!<On/Off Status of Elevator (To Display)
+        uint8 *pu8_elevatorButtonColor;         //!<Button color code for the Elevator On/Off button (To Button Panel)
+        //Ramp Control
+        T_RampParams t_ramp_params;
+        T_RampState  t_ramp_state;
+        float32 f32_safe_state;
 
-    //Control Checkpoints
-    T_ChkPoints_Elevator *pt_chkElevator;   //!<Elevator Control Checkpoints Structure
+        //RX CAN Variables
+        uint8 *pu8_requestedSpeed;              //!<Elevator Requested Speed (From Display)
+        uint8 *pu8_onOffCommand;                //!<Elevator On/Off Command (From Joystick)
+        //NVM Configuration Parameters
+        T_Config_Elevator *pt_nvmElevator;      //!<Elevator Control Configuration Structure
+
+        //NVM Configuration Parameters
+        T_Config_Elevator *pt_nvmElevator;      //!<Elevator Control Configuration Structure
+        //Control Checkpoints
+        T_ChkPoints_Elevator *pt_chkElevator;   //!<Elevator Control Checkpoints Structure
+
+        //Control Checkpoints
+        T_ChkPoints_Elevator *pt_chkElevator;   //!<Elevator Control Checkpoints Structure
+        // Toggle Button
+        T_ToggleBtn t_btn_enable;     //!< Toggle Button Control
 
 
 }T_ElevatorControl;
@@ -92,4 +102,3 @@ sint16 update_elevatorControl(void);
 
 
 #endif /* APPL_CORE_SRC_AGVWORK_ELEVATOR_CONTROL_H_ */
-
