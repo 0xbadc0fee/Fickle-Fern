9d5e7a9b1bde77bf0b8535700b2b25d55ff8d9f4
tiffanygohnert <tiffanygohnert@gmail.com>
2026-03-17 09:26:57 -0400

    FPC-155 Merge with develop branch

--- ESX_4CM_A/appl_core/src/System/IO/hw_outputs.c
+++ ESX_4CM_A/appl_core/src/System/IO/hw_outputs.c
@@ -34,7 +34,6 @@ T_VehicleOutput tvo_auto_unload =
     .f32_prevOutputValue = 0.0F,
     .mq_outputChanged = TRUE,
 
-<<<<<<< HEAD
     .u8_diagEnabled = FALSE,
     .t_fault = { .u8_dm1_enable = FALSE },
 
